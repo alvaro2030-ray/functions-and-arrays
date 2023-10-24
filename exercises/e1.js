c3bf60574898cb2318df9d88c63bcb71729f12ab
Alvaro Vasquez <vasquezalvaro004@gmail.com>
2023-10-24 13:08:18 -0400

    fixed e1.js

--- exercises/e1.js
+++ exercises/e1.js
@@ -5,8 +5,11 @@
 
 export function getFirstTenNumbers() {
   // Your code goes here...
-  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  return numbers;
+  var number = [];
+  for(let i = 1; i <= 10; i++){
+    number.push(i)
+}
+return number;
 }
 
 // === TEST YOURSELF ===
